84a2719221f141deb634d52f526195b9d78d10be
KhenCahyo13 <khencahyo02@gmail.com>
2022-10-21 07:38:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["pages/*.html"],
+  theme: {
+    fontFamily: {
+      'publicsans' : ['Public Sans', 'Sans Serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
